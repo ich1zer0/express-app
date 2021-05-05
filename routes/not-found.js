211be1387bf05f24fefdc70ba270621337d43088
徳永貴文 <proton.ich1@protonmail.com>
2021-05-05 15:57:14 +0900

    Change var -> const

--- routes/not-found.js
+++ routes/not-found.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.use((_, res) => {
   res.status(404).render('./not-found');
