b8dc96eb243f2b0ffc0b57ff7c86adce1d65c47b
徳永貴文 <proton.ich1@protonmail.com>
2021-05-23 13:35:08 +0900

    Add fetcher 追加

---
+++ public/javascripts/libs/fetcher.js
@@ -0,0 +1,18 @@
+const timeoutMs = 1000 * 10;
+
+const fetcher = async (requestUrl) => {
+  const abortController = new AbortController();
+  const { signal } = abortController;
+  setTimeout(() => abortController.abort(), timeoutMs);
+
+  try {
+    const response = await fetch(requestUrl, { signal });
+    return response.json();
+  } catch (error) {
+    return Promise.reject(
+      new Error(`${response.status}:${response.statusText}`)
+    );
+  }
+};
+
+export default fetcher;
