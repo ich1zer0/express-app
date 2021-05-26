7599fe1595537d761f1265499cb2a6bdbc335e0d
徳永貴文 <proton.ich1@protonmail.com>
2021-05-26 21:24:05 +0900

    Update Fetcher スクリプト更新

--- public/javascripts/libs/fetcher.js
+++ public/javascripts/libs/fetcher.js
@@ -1,18 +1,28 @@
+import HttpError from '../errors/http_error.js';
+import errorMessages from '../config/error_messages.js';
+
 const timeoutMs = 10 * 1000;
 
 const fetcher = async (requestUrl) => {
   const abortController = new AbortController();
   const { signal } = abortController;
-  setTimeout(() => abortController.abort(), timeoutMs);
+  const timeout = setTimeout(() => abortController.abort(), timeoutMs);
 
   try {
     const response = await fetch(requestUrl, { signal });
     if (!response.ok) {
-      throw new Error(response.statusText);
+      throw new HttpError(response.status, errorMessages[response.status]);
     }
     return response.json();
   } catch (error) {
-    return Promise.reject(error);
+    console.error(error);
+    if (error.name === 'AbortError') {
+      throw new HttpError(408, errorMessages[408]);
+    } else {
+      throw error;
+    }
+  } finally {
+    clearTimeout(timeout);
   }
 };
 
--- public/javascripts/libs/node-fetcher.js
+++ public/javascripts/libs/node-fetcher.js
@@ -3,22 +3,31 @@ const {
   abortableFetch,
 } = require('abortcontroller-polyfill/dist/cjs-ponyfill');
 const { fetch } = abortableFetch(require('node-fetch'));
+const NodeHttpError = require('../errors/node-http_error');
+const errorMessages = require('../config/node-error_messages');
 
 const timeoutMs = 10 * 1000;
 
 const fetcher = async (requestUrl) => {
   const abortController = new AbortController();
   const { signal } = abortController;
-  setTimeout(() => abortController.abort(), timeoutMs);
+  const timeout = setTimeout(() => abortController.abort(), timeoutMs);
 
   try {
     const response = await fetch(requestUrl, { signal });
     if (!response.ok) {
-      throw new Error(response.statusText);
+      throw new NodeHttpError(response.status, errorMessages[response.status]);
     }
     return response.json();
   } catch (error) {
-    return Promise.reject(error);
+    console.error(error);
+    if (error.name === 'AbortError') {
+      throw new NodeHttpError(408, errorMessages[408]);
+    } else {
+      throw error;
+    }
+  } finally {
+    clearTimeout(timeout);
   }
 };
 
